6cc0b13816d7789d82fa348ee84ae627eab8aa81
Anya <hannakilpatrick@gmail.com>
2018-05-07 11:12:30 -0400

    hookep repo with JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // requiring npm package
 var mysql = require("mysql");
-// setting up MySQL CONNECTION
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password:"y2n2sql-materdolorosa2",
-    database: "burgers_db"
-})
+
+// hooking project with JawsDB
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    // setting up MySQL CONNECTION
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env[2],
+        database: "burgers_db"
+    });
+}
+
 // making connection
 connection.connect(function(err){
     if(err) throw err;
