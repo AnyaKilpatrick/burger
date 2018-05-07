3d8933e5269b780fdff198ae8ac12e3229670693
Anya <hannakilpatrick@gmail.com>
2018-05-07 12:18:59 -0400

    added timestamp column

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,8 @@
 create database burgers_db;
 use burgers_db;
 create table burgers(
-    id INT primary key auto_increment,
-    burger_name VARCHAR(60) not null,
-    devoured BOOLEAN default false
+    id INT primary key auto_increment not null,
+    burger_name VARCHAR(100) not null,
+    devoured BOOLEAN default false,
+    createdAt TIMESTAMP default current_timestamp;
 );
